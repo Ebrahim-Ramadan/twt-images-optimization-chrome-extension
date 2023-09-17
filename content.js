c14ac896a16c3f9a0ee31d853142840d96d6e440
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-17 21:15:36 +0200

    initialized mutationObserver to catch a small number of images once js code is executed

--- content.js
+++ content.js
@@ -1,4 +1,13 @@
-const images = document.querySelectorAll('img');
-images.forEach((img) => {
-    console.log(img.src);
-  });
+const observer = new MutationObserver(() => {
+  const images = document.getElementsByClassName('css-9pa8cd'); 
+  console.log('images', images);
+  if (images.length > 0) {
+    for (let i = 0; i < images.length; i++) {
+      console.log(images[i].src);
+    }
+    observer.disconnect(); // Stop observing once images are found
+  }
+});
+
+// Start observing changes to the DOM
+observer.observe(document.body, { childList: true, subtree: true });
