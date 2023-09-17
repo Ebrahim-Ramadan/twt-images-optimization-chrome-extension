79eb125d7bcb71393286872bd796e2cad2a87597
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-17 21:25:05 +0200

    initialized mutationObserver to catch a small number for videos, too, same way

--- content.js
+++ content.js
@@ -1,13 +1,24 @@
 const observer = new MutationObserver(() => {
   const images = document.getElementsByClassName('css-9pa8cd'); 
+  const divs = document.querySelectorAll('div.css-1dbjc4n[data-testid="videoPlayer"]');
+
   console.log('images', images);
   if (images.length > 0) {
     for (let i = 0; i < images.length; i++) {
-      console.log(images[i].src);
+      console.log('images[i].src', images[i].src);
     }
     observer.disconnect(); // Stop observing once images are found
   }
+
+
+  console.log('divs', divs);
+  if (divs.length > 0) {
+    for (let i = 0; i < divs.length; i++) {
+      // Process the selected divs here
+      console.log('Div Content:', divs[i].textContent);
+    }
+  }
 });
 
-// Start observing changes to the DOM
+// Start observing changes to the DOM when js code is executed
 observer.observe(document.body, { childList: true, subtree: true });
